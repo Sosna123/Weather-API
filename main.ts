2474c2eabfa1f0c85c7a79593aad1c9bbb696d2f
Sosna123 <olek.arez@gmail.com>
2023-07-18 21:03:02 +0200

    Added way more info (and it works :D)

--- main.ts
+++ main.ts
@@ -1,27 +1,63 @@
 //html elements
 const cityNameText = document.querySelector("#cityNameText") as HTMLParagraphElement;
-const tempText = document.querySelector('p#tempText') as HTMLParagraphElement;
-const windSpeedText = document.querySelector('p#windSpeedText') as HTMLParagraphElement;
+const weatherDescText = document.querySelector("#weatherDesc") as HTMLParagraphElement;
+const weatherIconImg = document.querySelector("#weatherIcon") as HTMLImageElement;
+const tempText = document.querySelector('#tempText') as HTMLParagraphElement;
+const windSpeedText = document.querySelector('#windSpeedText') as HTMLParagraphElement;
+const pressureText = document.querySelector('#pressureText') as HTMLParagraphElement;
+const humidityText = document.querySelector('#humidityText') as HTMLParagraphElement;
 
-// object weather
-let weather = {
-    cityName: 'Konin',
-    displayWeather: (data) => {
-        const temp = data.main.temp;
-        const windSpeed = data.wind.speed;
+const cityInput = document.querySelector('#citySearch') as HTMLInputElement;
+const citySubmit = document.querySelector('#citySubmit') as HTMLButtonElement;
 
-        cityNameText.innerText = "City: " + this.cityName;
-        tempText.innerText = temp + "°C";
-        windSpeedText.innerText = "Wind speed: " + windSpeed + "m/s";
-    },
-    fetchWeather: (cityName) => {
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=d764b95fb2e4dbd50206ed6cae5b45a1`)
-        .then((response) => {response.json();})
-        .then((data) => this.displayWeather(data))
-    }
+// variables
+let cityName = 'Konin';
+let weatherDesc: string;
+let weatherIcon: string;
+let temp: number;
+let tempFeel: number;
+let windSpeed: number;
+let pressure: number;
+let humidity: number;
 
+// functions
+const fetchData = async () => {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=d764b95fb2e4dbd50206ed6cae5b45a1`);
+    const data = await response.json();
+    return data;
 }
 
-weather.fetchWeather(weather.cityName);
+const updateData = () => {
+    fetchData().then(data => {
+        cityName = data.name;
+        weatherDesc = data.weather[0].description;
+        weatherIcon = data.weather[0].icon;
+        temp = Math.floor(data.main.temp);
+        tempFeel = Math.floor(data.main.feels_like);
+        windSpeed = data.wind.speed;
+        pressure = data.main.pressure;
+        humidity = data.main.humidity
+        
+        cityNameText.innerText = `Weather in: ${cityName}`
+        weatherDescText.innerText = `${weatherDesc}`
+        weatherIconImg.src = `https://openweathermap.org/img/wn/${weatherIcon}.png`
+        tempText.innerText = `${temp}°C  (Feels like ${tempFeel}°C)`
+        windSpeedText.innerText = `Wind speed: ${windSpeed} m/s`
+        pressureText.innerText = `Pressure: ${pressure} hPa`
+        humidityText.innerHTML = `Humidity: ${humidity}%`
+    })
+}
+
+// setInterval(updateData, 10000)
+
+updateData();
+
+citySubmit.addEventListener("click", () => {
+    if(cityInput.value != ''){
+        cityName = cityInput.value;
+        cityInput.value = '';
+        updateData();
+    }
+})
 
 
